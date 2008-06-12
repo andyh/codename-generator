9a5813cad9f114e25c79725a95f3bbed6ef09907
Andy Henson <andy@elaptics.co.uk>
2008-06-12 23:51:29 +0100

    Loads in file specified by the command line argument and randomly generates 5 codewords

--- codename_generator.rb
+++ codename_generator.rb
@@ -1,7 +1,9 @@
+#!/usr/bin/env ruby -wKU
+
 class CodeNameGenerator
   def initialize(generator)
-    # load a generator
-    @codenames = %w{wolverine iceman cyclops storm rogue magneto}
+    # load a generator    
+    @codenames = File.open(File.dirname(__FILE__)+"/data/"+generator+".txt") {|t| t.readlines }
   end
   
   def generate
@@ -9,5 +11,12 @@ class CodeNameGenerator
   end
 end
 
-codename = CodeNameGenerator.new(nil)
-puts codename.generate
+unless ARGV.length == 1
+  puts "Usage: specify a codename list to generate from"
+  exit
+end
+
+codename = CodeNameGenerator.new(ARGV[0])
+5.times {
+  puts codename.generate  
+}
